cce41752997ebb5290ca7dc3156b7bd25da85962
badfilling <truemob2@gmail.com>
2018-03-17 15:27:15 +0100

    import project
    1st v

---
+++ TULbotpage/Controllers/DepartmentController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TULbotpage.Models;
+
+namespace TULbotpage.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private tulbotdevEntities db = new tulbotdevEntities();
+
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View(db.department.ToList());
+        }
+
+        // GET: Department/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            department department = db.department.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // GET: Department/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Department/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Short,Code,Website,Adress,Tel,Maps")] department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.department.Add(department);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(department);
+        }
+
+        // GET: Department/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            department department = db.department.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Department/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Short,Code,Website,Adress,Tel,Maps")] department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(department).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(department);
+        }
+
+        // GET: Department/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            department department = db.department.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Department/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            department department = db.department.Find(id);
+            db.department.Remove(department);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TULbotpage/Controllers/PeopleController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TULbotpage.Models;
+
+namespace TULbotpage.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private tulbotdevEntities db = new tulbotdevEntities();
+
+        // GET: People
+        public ActionResult Index()
+        {
+            return View(db.peoples.ToList());
+        }
+
+        // GET: People/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            peoples peoples = db.peoples.Find(id);
+            if (peoples == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peoples);
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,Name,Surname,Department,Consultation,Tel")] peoples peoples)
+        {
+            if (ModelState.IsValid)
+            {
+                db.peoples.Add(peoples);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(peoples);
+        }
+
+        // GET: People/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            peoples peoples = db.peoples.Find(id);
+            if (peoples == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peoples);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Name,Surname,Department,Consultation,Tel")] peoples peoples)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(peoples).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(peoples);
+        }
+
+        // GET: People/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            peoples peoples = db.peoples.Find(id);
+            if (peoples == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peoples);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            peoples peoples = db.peoples.Find(id);
+            db.peoples.Remove(peoples);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
